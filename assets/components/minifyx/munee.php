1dcf00d0d32c50a5529b89982bb0c71bf0171e9e
Bezumkin <bezumkin@yandex.ru>
2013-11-12 23:46:30 +0700

    Fixed another possible E_NOTICE

--- assets/components/minifyx/munee.php
+++ assets/components/minifyx/munee.php
@@ -3,8 +3,12 @@
 require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
 
-define('WEBROOT', MODX_BASE_PATH);
-define('MUNEE_CACHE', MODX_CORE_PATH . 'cache/default/munee/');
+if (!defined('WEBROOT')) {
+	define('WEBROOT', MODX_BASE_PATH);
+}
+if (!defined('MUNEE_CACHE')) {
+	define('MUNEE_CACHE', MODX_CORE_PATH . 'cache/default/munee/');
+}
 
 require_once MODX_CORE_PATH . 'components/minifyx/munee/munee.phar';
 echo \Munee\Dispatcher::run(new \Munee\Request());
