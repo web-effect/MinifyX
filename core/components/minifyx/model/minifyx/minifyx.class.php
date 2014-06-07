947dcaefa29ac58eb55f5466e8a35aff89ebf52b
Chero <manicko@yandex.ru>
2014-06-07 17:11:49 +0400

    Update minifyx.class.php
    $dir = str_replace('//', '/', '/' . $this->config['munee_cache'] .'/' ); 
    this version do not work on windows, so but 
    $dir = str_replace('//', '/', $this->config['munee_cache']); 
    works both on win and linux

--- core/components/minifyx/model/minifyx/minifyx.class.php
+++ core/components/minifyx/model/minifyx/minifyx.class.php
@@ -282,7 +282,7 @@ class MinifyX {
 	 * @return bool
 	 */
 	public function getTmpDir() {
-		$dir = str_replace('//', '/', '/' . $this->config['munee_cache'] . '/');
+		$dir = str_replace('//', '/', $this->config['munee_cache']); //works both on win and linux
 
 		if ($this->makeDir($dir)) {
 			return $dir;
